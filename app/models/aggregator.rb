ec6afd0fb3a466476453ac03583389a6e3429843
Carl Venezia <veneziacarl@gmail.com>
2016-02-19 14:12:26 -0800

    updated list of sites

--- app/models/aggregator.rb
+++ app/models/aggregator.rb
@@ -2,7 +2,21 @@ class Aggregator
   SITES = [
     { name: "Marijuana Venture", link: "http://www.marijuanaventure.com/feed/", description: "", articles: [] },
     { name: "The Daily Chronic", link: "http://www.thedailychronic.net/feed/", description: "", articles: [] },
-    { name: "Invest In Cannabis", link: "http://www.investincannabis.com/feed/", description: "", articles: [] }
+    { name: "Invest In Cannabis", link: "http://www.investincannabis.com/feed/", description: "", articles: [] },
+    { name: "Marijuana Business Daily", link: "http://mjbizdaily.com/feed/", description: "", articles: [] },
+    { name: "High Times", link: "https://feeds.feedburner.com/HIGHTIMESMagazine", description: "", articles: [] },
+    { name: "Cannabis Business Times", link: "http://www.cannabisbusinesstimes.com/rss", description: "", articles: [] },
+    { name: "Marijuana Stocks", link: "http://marijuanastocks.com/feed/", description: "", articles: [] },
+    { name: "Weed Worthy", link: "https://feeds.feedburner.com/Weedworthy-TodaysCannabisNewsNetwork", description: "", articles: [] },
+    { name: "Cannabis Culture", link: "http://www.cannabisculture.com/feed", description: "", articles: [] },
+    { name: "Investing News - Cannabis", link: "http://investingnews.com/category/daily/resource-investing/agriculture-investing/cannabis-investing/feed/", description: "", articles: [] },
+    { name: "The Weed Blog", link: "https://feeds.feedburner.com/TheWeedBlogcom", description: "", articles: [] },
+    { name: "Dope Magazine", link: "http://www.dopemagazine.com/feed/", description: "", articles: [] },
+    { name: "New Cannabis Ventures", link: "https://www.newcannabisventures.com/feed/", description: "", articles: [] },
+    { name: "Ganjapreneur", link: "http://www.ganjapreneur.com/feed/", description: "", articles: [] },
+    { name: "Cannabis-Chronicles", link: "http://cannabis-chronicles.com/feed/", description: "", articles: [] },
+    { name: "Cashinbis", link: "https://feeds.feedburner.com/cashinbis", description: "", articles: [] },
+    { name: "Marijuana", link: "http://www.marijuana.com/feed/", description: "", articles: [] }
   ]
 
   def self.generate_news
@@ -12,7 +26,15 @@ class Aggregator
       news << site
       rss = RSS::Parser.parse(site[:link], false)
       rss.items.each do |item|
-        if item.date >= today
+        if site[:link].match(/feedburner/)
+          if item.date == nil
+            if item.dc_date >= today
+              news[index][:articles] << { title: item.title, description: item.description, link: item.link }
+            end
+          elsif item.date >= today
+            news[index][:articles] << { title: item.title, description: item.description, link: item.link }
+          end
+        elsif item.date >= today
           news[index][:articles] << { title: item.title, description: item.description, link: item.link }
         end
       end
